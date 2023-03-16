3cfd27d250e0d863e6c4a178b2f62c9683cc3d69
Dmitry <dima.korovashev@mail.ru>
2023-03-16 16:02:12 +0300

    master

---
+++ 4 laba/laba4.py
@@ -0,0 +1,39 @@
+def num1(chislo):
+    return chislo % 3 == 0
+chislo = int(input("Введите число: "))
+print(num1(chislo))
+
+def zadacha2():
+    def num2(chislo):
+        return 100 / chislo
+    try:
+        chislo = int(input("Введите число: "))
+        print(num1(num2(chislo)))
+    except ValueError:
+        print("Это не число.")
+    except ZeroDivisionError:
+        print("На ноль делить нельзя!")
+
+def zadacha3():
+        do = input("Введите дату: ")
+        dat = do.split(".")
+        return int(dat[0]) * int(dat[1]) == int(dat[2][2:4])
+        print(do)
+
+def zadacha4():
+        sum1,sum2 = 0,0
+        da = input("Введите номер билета: ")
+        ser = int(len(da)/2)
+        for i in range(ser):
+            sum1 += int(da[i])
+        for i in range(ser, len(da)):
+            sum2 += int(da[i])
+        if sum1 == sum2:
+           print("Билет счастливый")
+        else:
+           print("Билет не счастливый")
+        print(da)
+
+zadacha2()
+zadacha3()
+zadacha4()
